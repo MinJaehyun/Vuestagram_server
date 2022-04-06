c9513fe1038baf25b871ecada9e1136e5f44f5fa
{krism01} <{krism01@naver.com}>
2022-04-06 22:31:25 +0900

    src 구조 설정 1. app.js 2. api/ 3. models/

---
+++ src/models/UserModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      maxlength: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      maxlength: 50,
+    },
+    insertedData: {
+      type: Date,
+      default: Date.now
+    },
+  },
+);
+
+const UserModel = mongoose.model('Users', userSchema);
+
+export default UserModel;
