783abf3e0d4130d72c609fb226b27595f8703f5e
{krism01} <{krism01@naver.com}>
2022-04-08 22:34:54 +0900

    index.js 로 명칭 변경 및 고정 설정한 port 를 process.env 로 설정하여 Error R10 (Boot timeout) 에러 해결

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// libs
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+
+// api
+import auth from './api/auth.js';
+
+// dotenv setup
+dotenv.config();
+
+// mongoDB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+mongoose.connect(
+	`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.geek9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+);
+
+// server setup
+const PORT = process.env.PORT
+
+// express setup
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('dev')); // log request
+
+// test 
+app.get("/", (req, res) => {
+	res.send({ hello: "world" });
+});
+
+// express routers
+app.use('/auth', auth);
+
+// server start
+app.listen(PORT, () => {
+	console
+		.log(`${chalk.white
+		.bgHex('#41b883')
+		.bold(`VUESTAGRAM SERVER IS RUNNING ON ${PORT}`)}`
+		)
+});
