1ada59485366a99262e42b14579237293c874b98
{krism01} <{krism01@naver.com}>
2022-04-10 15:51:29 +0900

    test async await authSignup.js

---
+++ src/api/testAuthSignup.js
@@ -0,0 +1,33 @@
+// libs
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+
+// modules
+import UserModel from '../models/UserModel.js';
+
+// router init
+const router = Router();
+
+// router
+router.post('/signup', async (req, res) => {
+	const { username, password, nickname } = req.body;
+	try {
+		let user = await UserModel.findOne({ username });
+		if (user) {
+			return res.status(409).send('이미 존재하는 id 입니다.');
+		}
+		const newUser = new UserModel({
+			username,
+			password,
+			nickname,
+		});
+		newUser.password = await bcrypt.hash(password, 10);
+		await newUser.save();
+		res.send(newUser);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+export default router;
