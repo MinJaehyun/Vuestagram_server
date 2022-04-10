1b6ff56f03b54541a8b14ac57d51803829389977
{krism01} <{krism01@naver.com}>
2022-04-10 10:51:21 +0900

    Apply eslint

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,31 +1,29 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      maxlength: 50,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    nickname: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      maxlength: 50,
-    },
-    insertedData: {
-      type: Date,
-      default: Date.now
-    },
-  },
-);
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		maxlength: 50,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	nickname: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		maxlength: 50,
+	},
+	insertedData: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
 const UserModel = mongoose.model('Users', userSchema);
 
