18a164f97a1e70d3e63696dd26341da01df01feb
{krism01} <{krism01@naver.com}>
2022-04-11 22:58:32 +0900

    if(err)로 status 를 출력하지 못하여, async await 적용 및 try catch 사용하여 해결

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,35 +9,24 @@ import UserModel from '../models/UserModel.js';
 const router = Router();
 
 // router
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
   const { username, password, nickname } = req.body;
-  bcrypt.hash(password, 10, (error, hashedPassword) => {
-    try {
-      const newUser = new UserModel({
-        username,
-        password: hashedPassword,
-        nickname,
-      });
-      newUser.save((error, saved) => {
-        if (error) {
-          // 경우 1. res.status(409).send(); // status(409) 를 나타내므로 뒤에 send 를 출력하지 못한다.
-          // 경우 2. res.send().status(409);
-          /** FIXME: status(409) 설정 시,
-           * server 값을 client 에서 잘 수신 했으므로 200 나타내며,
-           * client 에서 server 로 요청 시, id 충돌로 409 나타내도록,
-           * 로직 작성하였는 흐름이 맞는지? */
-          console.log(error);
-          res.send('존재하는 id 입니다').status(409);
-        } else {
-          console.log(saved);
-          res.send(saved);
-        }
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error });
+  if (!username || !password)
+    return res.status(400).send({ err: 'Require nickname and password' });
+
+  try {
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      return res.status(409).send('이미 존재하는 id 입니다.');
     }
-  });
+    const newUser = new UserModel({ username, password, nickname });
+    newUser.password = await bcrypt.hash(password, 10);
+    await newUser.save();
+    return res.status(201).send(newUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: error.message });
+  }
 });
 
 export default router;
