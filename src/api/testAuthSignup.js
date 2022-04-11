56d0d67d16596e5cc7a313935948eb39ab1ea615
{krism01} <{krism01@naver.com}>
2022-04-11 15:58:07 +0900

    make then() to async await

--- src/api/testAuthSignup.js
+++ src/api/testAuthSignup.js
@@ -10,24 +10,24 @@ const router = Router();
 
 // router
 router.post('/signup', async (req, res) => {
-	const { username, password, nickname } = req.body;
-	try {
-		let user = await UserModel.findOne({ username });
-		if (user) {
-			return res.status(409).send('이미 존재하는 id 입니다.');
-		}
-		const newUser = new UserModel({
-			username,
-			password,
-			nickname,
-		});
-		newUser.password = await bcrypt.hash(password, 10);
-		await newUser.save();
-		res.send(newUser);
-	} catch (error) {
-		console.error(error.message);
-		res.status(500).send('Server Error');
-	}
+  const { username, password, nickname } = req.body;
+  try {
+    let user = await UserModel.findOne({ username });
+    if (user) {
+      return res.status(409).send('이미 존재하는 id 입니다.');
+    }
+    const newUser = new UserModel({
+      username,
+      password,
+      nickname,
+    });
+    newUser.password = await bcrypt.hash(password, 10);
+    await newUser.save();
+    res.send(newUser);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 export default router;
