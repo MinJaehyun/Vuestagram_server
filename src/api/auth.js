1fb0817777b0ddbe08c1f9e85e20858bc4619ce8
{krism01} <{krism01@naver.com}>
2022-04-12 13:21:21 +0900

    fix '

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,7 +9,7 @@ import { newToken } from '../utils/auth.js';
 // router init
 const router = Router();
 
-// router
+// Login Router
 router.post('/login', async (req, res) => {
   // id & pw를 DB 에서 검증하여 같으면 진행, 다르면 에러 출력
   // 로그인 성공 시, 토큰을 server 로부터 받아 client 에 저장하기
@@ -40,6 +40,27 @@ router.post('/login', async (req, res) => {
   } catch (err) {
     if (err) res.status(500).send('Internal Server Error');
     throw err;
+  }
+});
+
+// Signup Router
+router.post('/signup', async (req, res) => {
+  const { username, password, nickname } = req.body;
+  if (!username || !password)
+    return res.status(400).send({ err: 'Require nickname and password' });
+
+  try {
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      return res.status(409).send({ err: '이미 존재하는 id 입니다.' });
+    }
+    const newUser = new UserModel({ username, password, nickname });
+    newUser.password = await bcrypt.hash(password, 10);
+    await newUser.save();
+    return res.status(201).send(newUser);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({ err: err.message });
   }
 });
 
