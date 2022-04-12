7306d2fba44399e4d6e2e41bd33bb4371a2cd66b
{krism01} <{krism01@naver.com}>
2022-04-12 12:24:39 +0900

    test 5_login_promise

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,11 +4,47 @@ import bcrypt from 'bcrypt';
 
 // modules
 import UserModel from '../models/UserModel.js';
+import { newToken } from '../utils/auth.js';
 
 // router init
 const router = Router();
 
 // router
+router.post('/login', async (req, res) => {
+  // id & pw를 DB 에서 검증하여 같으면 진행, 다르면 에러 출력
+  // 로그인 성공 시, 토큰을 server 로부터 받아 client 에 저장하기
+  try {
+    const { username, password } = req.body;
+    // id check
+    const user = await UserModel.findOne({ username: username });
+    if (!user) {
+      res.status(401).send({ err: 'Authentication failed. User not found.' });
+    }
+    // pw check
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) res.status(500).send('Internal Server Error');
+      if (result) {
+        const token = newToken(user);
+        const loggedUser = {
+          username: user.username,
+          nickname: user.nickname,
+        };
+        res.status(200).json({
+          success: true,
+          user: loggedUser,
+          message: 'Login Success',
+          token: token,
+        });
+      } else {
+        res.status(401).json('Authentication failed. Wrong password.');
+      }
+    });
+  } catch (err) {
+    res.status(500).json('Internal Server Error');
+    throw err;
+  }
+});
+
 router.post('/signup', async (req, res) => {
   const { username, password, nickname } = req.body;
   if (!username || !password)
