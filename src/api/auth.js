1d8543a11575682c8ef70a4f3267b4e3ae890e10
{krism01} <{krism01@naver.com}>
2022-04-12 12:27:28 +0900

    login promise 구조

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,40 +4,65 @@ import bcrypt from 'bcrypt';
 
 // modules
 import UserModel from '../models/UserModel.js';
+import { newToken } from '../utils/auth.js';
 
 // router init
 const router = Router();
 
 // router
-router.post('/signup', (req, res) => {
-  const { username, password, nickname } = req.body;
-  bcrypt.hash(password, 10, (error, hashedPassword) => {
-    try {
-      const newUser = new UserModel({
-        username,
-        password: hashedPassword,
-        nickname,
-      });
-      newUser.save((error, saved) => {
-        if (error) {
-          // 경우 1. res.status(409).send(); // status(409) 를 나타내므로 뒤에 send 를 출력하지 못한다.
-          // 경우 2. res.send().status(409);
-          /** FIXME: status(409) 설정 시,
-           * server 값을 client 에서 잘 수신 했으므로 200 나타내며,
-           * client 에서 server 로 요청 시, id 충돌로 409 나타내도록,
-           * 로직 작성하였는 흐름이 맞는지? */
-          console.log(error);
-          res.send('존재하는 id 입니다').status(409);
-        } else {
-          console.log(saved);
-          res.send(saved);
-        }
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error });
+router.post('/login', async (req, res) => {
+  // id & pw를 DB 에서 검증하여 같으면 진행, 다르면 에러 출력
+  // 로그인 성공 시, 토큰을 server 로부터 받아 client 에 저장하기
+  try {
+    const { username, password } = req.body;
+    // id check
+    const user = await UserModel.findOne({ username: username });
+    if (!user) {
+      res.status(401).send({ err: 'Authentication failed. User not found.' });
     }
-  });
+    // pw check
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) res.status(500).send('Internal Server Error');
+      if (result) {
+        const token = newToken(user);
+        const loggedUser = {
+          username: user.username,
+          nickname: user.nickname,
+        };
+        res.status(200).json({
+          success: true,
+          user: loggedUser,
+          message: 'Login Success',
+          token: token,
+        });
+      } else {
+        res.status(401).json('Authentication failed. Wrong password.');
+      }
+    });
+  } catch (err) {
+    res.status(500).json('Internal Server Error');
+    throw err;
+  }
+});
+
+router.post('/signup', async (req, res) => {
+  const { username, password, nickname } = req.body;
+  if (!username || !password)
+    return res.status(400).send({ err: 'Require nickname and password' });
+
+  try {
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      return res.status(409).send({ err: '이미 존재하는 id 입니다.' });
+    }
+    const newUser = new UserModel({ username, password, nickname });
+    newUser.password = await bcrypt.hash(password, 10);
+    await newUser.save();
+    return res.status(201).send(newUser);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({ err: err.message });
+  }
 });
 
 export default router;
